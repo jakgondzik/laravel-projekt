a37fa4f404e3fd5f565610b036363ca42bc3f172
jakgondzik <149070003+jakgondzik@users.noreply.github.com>
2024-01-14 21:32:46 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ Route::get('/', function () {
     ], 200);
 });
 $student_data = "/gondzik/310568";
-Route::get("$student_data/users", UsersController::class . '@index');
-Route::get("$student_data/user/{id}", UsersController::class . '@show');
-Route::post("$student_data/user", UsersController::class . '@store');
-Route::put("$student_data/user/{id}", UsersController::class . '@update');
-Route::delete("$student_data/user/{id}", UsersController::class . '@destroy');
+Route::get("$student_data/peoples", UsersController::class . '@index');
+Route::get("$student_data/people/{id}", UsersController::class . '@show');
+Route::post("$student_data/people", UsersController::class . '@store');
+Route::put("$student_data/people/{id}", UsersController::class . '@update');
+Route::delete("$student_data/people/{id}", UsersController::class . '@destroy');
